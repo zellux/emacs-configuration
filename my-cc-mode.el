2e2c9841a41271892db8c2bc8e44af4abacebf00
Wang Yuanxuan <devnull@localhost>
2010-02-04 23:49:56 +0800

    Add a cscope-friendly layout

--- my-cc-mode.el
+++ my-cc-mode.el
@@ -264,9 +264,20 @@
 (add-to-list 'load-path "~/emacs/cedet-1.0pre6/speedbar")
 (add-to-list 'load-path "~/emacs/cedet-1.0pre6/eieio")
 
-(require 'ecb-autoloads)
+(require 'ecb)
 
-(setq ecb-layout-name "left11")
+(ecb-layout-define "my-cscope-layout" left nil
+                   (ecb-set-methods-buffer)
+                   (ecb-split-ver 0.5 t)
+                   (other-window 1)
+                   (ecb-set-history-buffer)
+                   (ecb-split-ver 0.2 t)
+                   (other-window 1)
+                   (ecb-set-cscope-buffer))
+
+(defecb-window-dedicator ecb-set-cscope-buffer " *ECB cscope-buf*"
+                         (switch-to-buffer "*cscope*"))
+(setq ecb-layout-name "my-cscope-layout")
 
 ;; Compilation
 (setq compilation-scroll-output t)
