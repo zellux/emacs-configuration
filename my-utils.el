6f7d0a79011dbaea7d725a14c976becbda04bd19
Wang Yuanxuan <zellux@gmail.com>
2010-03-17 15:07:16 +0800

    ibuffer and uniquify settings

--- my-utils.el
+++ my-utils.el
@@ -26,3 +26,49 @@
 (load "ocaml.emacs")
 
 ;; (load "quack")
+
+;; ibuffer for buffer management
+(require 'ibuffer) 
+(setq ibuffer-saved-filter-groups
+  (quote (("default"      
+            ("Org" ;; all org-related buffers
+              (mode . org-mode))  
+            ("Mail"
+              (or  ;; mail-related buffers
+               (mode . message-mode)
+               (mode . mail-mode)
+               ;; etc.; all your mail related modes
+               ))
+            ("Danimos"
+              (filename . "danimos/"))
+            ("Programming" ;; prog stuff not already in MyProjectX
+              (or
+                (mode . c-mode)
+                (mode . perl-mode)
+                (mode . python-mode)
+                (mode . emacs-lisp-mode)
+                ;; etc
+                )) 
+            ("ERC"   (mode . erc-mode))))))
+(("default" ("Org" (mode . org-mode)) ("Mail" (or ... ...)) ("Danimos" (filename . "danimos/")) ("Programming" (or ... ... ... ...)) ("ERC" (mode . erc-mode))))
+
+
+(add-hook 'ibuffer-mode-hook
+  (lambda ()
+    (ibuffer-switch-to-saved-filter-groups "default")))
+((lambda nil (ibuffer-switch-to-saved-filter-groups "default")))
+
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(setq ibuffer-show-empty-filter-groups nil)
+
+(add-hook 'ibuffer-mode-hook 
+	  '(lambda ()
+	     (ibuffer-auto-mode 1)
+	     (ibuffer-switch-to-saved-filter-groups "home")))
+
+;; Make buffer name unique
+(require 'uniquify) 
+(setq
+  uniquify-buffer-name-style 'post-forward
+  uniquify-separator ":")
