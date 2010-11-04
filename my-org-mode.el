cda29b9dc54a2b4a1391c3b539ccf50823b33b33
Wang Yuanxuan <zellux@gmail.com>
2010-11-05 01:34:23 +0800

    update org mode to 7.3

--- my-org-mode.el
+++ my-org-mode.el
@@ -1,11 +1,18 @@
 ;; org-mode
-(setq load-path (cons "~/emacs/org-6.34a/contrib/lisp" load-path))
+(setq load-path (cons "~/emacs/org-7.3/contrib/lisp" load-path))
+(setq load-path (cons "~/emacs/org-7.3/lisp" load-path))
 (require 'org-install)
+(require 'org-publish)
+
 
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
+
+(add-hook 'org-mode-hook 'turn-on-font-lock)
+(add-hook 'org-mode-hook 
+(lambda () (setq truncate-lines nil)))
 (setq org-log-done t)
-(setq org-agenda-files (list "~/org/todo.org" "~/org/misc.org"))
+(setq org-agenda-files (list "~/Documents/org/todo.org" "~/Documents/org/misc.org"))
 
-
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
