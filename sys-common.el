bfbe18e01fc9b23e676fd3fd0e6fc03d3f933e99
Wang Yuanxuan <zellux@gmail.com>
2010-11-15 14:22:13 +0800

    Move common settings to sys-common.el

---
+++ sys-common.el
@@ -0,0 +1,33 @@
+;; Common settings
+
+(setq backup-directory-alist '(("" . "~/.emacs.d/.backups")))
+;; Marking regions by C-2
+(global-set-key (kbd "C-2") 'set-mark-command)
+
+;; Highlight FIXME, TODO and BUG
+(font-lock-add-keywords nil
+			'(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))
+
+;; Alternative for show-paren-mode
+(require 'mic-paren)
+(paren-activate)
+
+;; Syntax highlight
+(global-font-lock-mode t)
+
+;; Bookmark
+(setq 
+ bookmark-default-file "~/.emacs.d/bookmarks" ;; keep my ~/ clean
+ bookmark-save-flag 1)                        ;; autosave each change)
+
+;; Color theme
+(setq load-path (cons "~/emacs/utils/color-themes" load-path))
+(require 'color-theme)
+(load "color-theme-tango.el")
+(load "color-theme-wyx.el")
+
+(color-theme-wyx)
+
+;; Column display ON
+(column-number-mode t)
+
