3490fd587984b909cf69866ba7a9f0a0fcd17313
Wang Yuanxuan <zellux@gmail.com>
2011-02-04 13:04:45 +0800

    lang-scala.el

---
+++ lang-scala.el
@@ -0,0 +1,9 @@
+(add-to-list 'load-path "~/emacs/langmode/scala-mode")
+(require 'scala-mode-auto)
+(yas/load-directory "~/emacs/langmode/scala-mode/contrib/yasnippet/snippets")
+
+(add-hook 'scala-mode-hook
+          '(lambda ()
+             (yas/minor-mode-on)
+             ))
+
