ee92596040c1d6bf402b05777794c0d19dcd77de
Wang Yuanxuan <zellux@gmail.com>
2011-03-14 20:23:18 +0800

    Forgot to add lang-ruby.el last time

---
+++ lang-ruby.el
@@ -0,0 +1,18 @@
+;; Inferior Ruby mode
+(add-to-list 'load-path (expand-file-name "~/emacs/langmode/ruby/ruby"))
+(autoload 'run-ruby "inf-ruby"
+  "Run an inferior Ruby process")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+(add-hook 'ruby-mode-hook
+      '(lambda ()
+         (inf-ruby-keys)
+))
+
+;; Emacs-rails
+(add-to-list 'load-path (expand-file-name "~/emacs/langmode/ruby/emacs-rails"))
+(require 'rails)
+
+;; RHTML-mode
+(add-to-list 'load-path (expand-file-name "~/emacs/langmode/ruby/rhtml"))
+(require 'rhtml-mode)
