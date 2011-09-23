97d2220028b3d199f53833ca0a3a59509f30c0fd
Yuanxuan Wang <zellux@gmail.com>
2011-09-23 20:42:15 +0800

    Add rhtml and rails support

--- lang-ruby.el
+++ lang-ruby.el
@@ -18,3 +18,27 @@
 ;; RHTML-mode
 (add-to-list 'load-path (expand-file-name "~/emacs/langmode/ruby/rhtml"))
 (require 'rhtml-mode)
+
+(autoload 'ruby-mode "ruby-mode" nil t)
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+(add-hook 'ruby-mode-hook '(lambda ()
+                             (setq ruby-deep-arglist t)
+                             (setq ruby-deep-indent-paren nil)
+                             (setq c-tab-always-indent nil)
+                             (require 'inf-ruby)
+                             (require 'ruby-compilation))))
+
+(autoload 'rhtml-mode "rhtml-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.rjs\\'" . rhtml-mode))
+(add-hook 'rhtml-mode '(lambda ()
+                         (define-key rhtml-mode-map (kbd "M-s") 'save-buffer))))
+
+(autoload 'yaml-mode "yaml-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode)))
