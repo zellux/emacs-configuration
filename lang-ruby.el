d204a81e024ed2a5684f0a4b3907d7702ab3c1da
Yuanxuan Wang <zellux@gmail.com>
2011-10-09 14:43:05 +0800

    Disable flymake warning

--- lang-ruby.el
+++ lang-ruby.el
@@ -17,28 +17,25 @@
 
 ;; RHTML-mode
 (add-to-list 'load-path (expand-file-name "~/emacs/langmode/ruby/rhtml"))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.rjs\\'" . rhtml-mode))
 (require 'rhtml-mode)
 
-(autoload 'ruby-mode "ruby-mode" nil t)
 (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
-(add-hook 'ruby-mode-hook '(lambda ()
-                             (setq ruby-deep-arglist t)
-                             (setq ruby-deep-indent-paren nil)
-                             (setq c-tab-always-indent nil)
-                             (require 'inf-ruby)
-                             (require 'ruby-compilation))))
-
-(autoload 'rhtml-mode "rhtml-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . rhtml-mode))
-(add-to-list 'auto-mode-alist '("\\.rjs\\'" . rhtml-mode))
-(add-hook 'rhtml-mode '(lambda ()
-                         (define-key rhtml-mode-map (kbd "M-s") 'save-buffer))))
 
 (autoload 'yaml-mode "yaml-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode)))
+(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+(defadvice flymake-start-syntax-check-process (after
+                                               cheeso-advice-flymake-start-syntax-check-1
+                                               (cmd args dir)
+                                               activate compile)
+  ;; set flag to allow exit without query on any
+  ;;active flymake processes
+  (set-process-query-on-exit-flag ad-return-value nil))
