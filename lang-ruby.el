21e3af8ff73b463e39556288c70e7c26b133b492
Yuanxuan Wang <zellux@gmail.com>
2012-01-14 18:19:16 +0800

    2-space-tab for coffee script

--- lang-ruby.el
+++ lang-ruby.el
@@ -49,3 +49,8 @@
 
 ;; coffee-mode
 (require 'coffee-mode)
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (set (make-local-variable 'tab-width) 2))
+(add-hook 'coffee-mode-hook  '(lambda() (coffee-custom)))
+
