759d469b81e740eb2d62b18b72868cfeb403e58e
Yuanxuan Wang <zellux@gmail.com>
2012-01-27 21:58:07 +0800

    Tweak emacs-rails configurations

--- lang-ruby.el
+++ lang-ruby.el
@@ -49,3 +49,11 @@
 
 ;; coffee-mode
 (require 'coffee-mode)
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+(setq ruby-deep-indent-paren nil)
