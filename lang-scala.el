51a103aed2c72d85a78f7899f1ca0fa7283f4262
Yuanxuan Wang <zellux@gmail.com>
2012-05-27 15:01:03 +0800

    Disable yas for scala

--- lang-scala.el
+++ lang-scala.el
@@ -1,8 +1,8 @@
 (add-to-list 'load-path "~/emacs/langmode/scala-mode")
 (require 'scala-mode-auto)
-(yas/load-directory "~/emacs/langmode/scala-mode/contrib/yasnippet/snippets")
+;; (yas/load-directory "~/emacs/langmode/scala-mode/contrib/yasnippet/snippets")
 
-(add-hook 'scala-mode-hook
-          '(lambda ()
-             (yas/minor-mode-on)
-             ))
+;; (add-hook 'scala-mode-hook
+;;           '(lambda ()
+;;              (yas/minor-mode-on)
+;;              ))
