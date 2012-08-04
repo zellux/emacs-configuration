15ece0a05de21e092116e31388aea21f6d4fae91
Yuanxuan Wang <zellux@gmail.com>
2012-08-04 12:52:37 +0800

    Ask before quitting

--- sys-common.el
+++ sys-common.el
@@ -48,3 +48,14 @@
   (message "Console"))
  (t (load "gui-settings.el")))
 
+;; Ask before quitting
+(defun ask-before-closing ()
+  "Ask whether or not to close, and then close if y was pressed"
+  (interactive)
+  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
+      (if (< emacs-major-version 22)
+          (save-buffers-kill-terminal)
+        (save-buffers-kill-emacs))
+    (message "Canceled exit")))
+
+(global-set-key (kbd "C-x C-c") 'ask-before-closing)
