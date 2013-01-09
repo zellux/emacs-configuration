c3908ec49da63bc6905d4bbb75c4c6541b086544
Yuanxuan Wang <zellux@gmail.com>
2013-01-09 17:14:38 +0800

    Enable cscope for Java files

--- lang-java.el
+++ lang-java.el
@@ -2,3 +2,5 @@
 ; load the smali/baksmali mode
 (autoload 'smali-mode "smali-mode" "Major mode for editing and viewing smali issues" t)
 (setq auto-mode-alist (cons '(".smali$" . smali-mode) auto-mode-alist))
+
+(add-hook 'java-mode-hook (function cscope:hook))
