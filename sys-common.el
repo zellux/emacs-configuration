e3955bb397d1a4cbc243418e9eb7d78b481eb30f
Yuanxuan Wang <zellux@gmail.com>
2013-06-30 22:02:29 +0800

    Move customized faces to sys-common

--- sys-common.el
+++ sys-common.el
@@ -70,3 +70,12 @@
 (add-to-list 'load-path "~/emacs/utils/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bm-face ((t (:background "color-236" :foreground "white"))))
+ '(ecb-default-highlight-face ((t (:background "brightgreen"))))
+ '(semantic-decoration-on-private-members-face ((((class color) (background dark)) (:background "#2e3330")))))
