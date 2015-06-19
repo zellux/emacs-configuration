ecd8f2ca35019b598f076d9dc76c254d165dee45
Zellux Wang <zellux@gmail.com>
2015-06-19 14:04:55 -0700

    Gofmt before saving

--- lang-go.el
+++ lang-go.el
@@ -3,10 +3,14 @@
 
 (defun go-mode-custom ()
   (set (make-local-variable 'tab-width) 2)
-  (set (make-local-variable 'indent-tabs-mode) nil))
+  (set (make-local-variable 'indent-tabs-mode) nil)
+  (add-hook 'before-save-hook 'gofmt-before-save))
 
 
 (add-hook 'go-mode-hook
       '(lambda ()
-         (go-mode-custom)
-))
+         (go-mode-custom)))
+
+(let ((gopath (concat (getenv "GOPATH") "/src/golang.org/x/tools/cmd/oracle/oracle.el")))
+  (if (file-exists-p gopath)
+      (load-file gopath)))
